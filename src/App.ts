3e4e5795642facfba9d2c01d3438d9d26d7bca85
DavidKim0715 <sunkit0526@gmail.com>
2021-12-22 22:55:20 +0900

    UPDATE - imported component

--- src/App.ts
+++ src/App.ts
@@ -1,20 +1,24 @@
-// LINK components
-
+// LINK components=
+import PageCarousel from "./components/carousel/PageCarousel";
+import PageFooter from "./components/layout/PageFooter";
+import PageMenuBar from "./components/bar/PageMenuBar";
+import PageHeader from "./components/layout/PageHeader";
 //component example
-// export const renderCommentsTitle = () => {
-//   document.querySelector('.comments-title')!.innerHTML = CommentsHeader();
-// };
+export const renderPageHeader = () => {
+    document.querySelector('.page-header')!.innerHTML = PageHeader();
+};
 
-// export const renderCommentsList = async () => {
-//   document.querySelector('.comments-list')!.innerHTML = await CommentsList();
-// };
+export const renderPageFooter = async () => {
+    document.querySelector('.page-footer')!.innerHTML = PageFooter();
+    // document.querySelector('.page-footer')!.innerHTML = await PageHeader();
+};
 
-// export const renderCommentsForm = () => {
-//   document.querySelector('.comments-form')!.appendChild(CommentsForm());
-// };
+export const renderPageCarousel = () => {
+    document.querySelector('.page-carousel')!.appendChild(PageCarousel());
+};
 
-// export const renderCommentsPagination = () => {
-//   document
-//     .querySelector('.comments-pagination')!
-//     .appendChild(CommentsPagination());
-// };
+export const renderMenuBar = () => {
+    document
+        .querySelector('.page-menubar')!
+        .appendChild(PageMenuBar());
+};
---
+++ src/components/carousel/PageCarousel.ts
@@ -0,0 +1,10 @@
+const PageCarousel = () => {
+    const template = `
+    <section id="carousel" class="navbar neon-title gradient px-3">
+      <div class="container-fluid justify-content-center">
+        <span class=""></span>
+      </div>
+    </section>`
+    return template
+}
+export default PageCarousel
