98d19c9fbb694cfa1789baaacd9882f0df06ba63
DavidKim0715 <sunkit0526@gmail.com>
2021-12-27 16:59:56 +0900

    UPDATE - 라우트 추가

---
+++ src/components/layout/PageHeader.ts
@@ -0,0 +1,10 @@
+const PageHeader = () => {
+    const template = `
+    <header id="header" class="navbar neon-title gradient px-3">
+      <div class="container-fluid justify-content-center">
+        <span class=""></span>
+      </div>
+    </header>`
+    return template
+}
+export default PageHeader
